cb9fba2ba80aaf8422ad9b34c899108ae52869ff
Alexis Do Nascimento <alexis.dona@gmail.com>
2020-01-14 22:00:06 -0300

    punto1 y punto2 resueltos

--- js/pixel-art.js
+++ js/pixel-art.js
@@ -1,35 +1,56 @@
 var nombreColores = ['White', 'LightYellow',
-  'LemonChiffon', 'LightGoldenrodYellow', 'PapayaWhip', 'Moccasin', 'PeachPuff', 'PaleGoldenrod', 'Bisque', 'NavajoWhite', 'Wheat', 'BurlyWood', 'Tan',
-  'Khaki', 'Yellow', 'Gold', 'Orange', 'DarkOrange', 'OrangeRed', 'Tomato', 'Coral', 'DarkSalmon', 'LightSalmon', 'LightCoral', 'Salmon', 'PaleVioletRed',
-  'Pink', 'LightPink', 'HotPink', 'DeepPink', 'MediumVioletRed', 'Crimson', 'Red', 'FireBrick', 'DarkRed', 'Maroon',
-  'Brown', 'Sienna', 'SaddleBrown', 'IndianRed', 'RosyBrown',
-  'SandyBrown', 'Goldenrod', 'DarkGoldenrod', 'Peru',
-  'Chocolate', 'DarkKhaki', 'DarkSeaGreen', 'MediumAquaMarine',
-  'MediumSeaGreen', 'SeaGreen', 'ForestGreen', 'Green', 'DarkGreen', 'OliveDrab', 'Olive', 'DarkOliveGreen', 'YellowGreen', 'LawnGreen',
-  'Chartreuse', 'GreenYellow', 'Lime', 'SpringGreen', 'LimeGreen',
-  'LightGreen', 'PaleGreen', 'PaleTurquoise',
-  'AquaMarine', 'Cyan', 'Turquoise', 'MediumTurquoise', 'DarkTurquoise', 'DeepSkyBlue',
-  'LightSeaGreen', 'CadetBlue', 'DarkCyan', 'Teal', 'Steelblue', 'LightSteelBlue', 'Honeydew', 'LightCyan',
-  'PowderBlue', 'LightBlue', 'SkyBlue', 'LightSkyBlue',
-  'DodgerBlue', 'CornflowerBlue', 'RoyalBlue', 'SlateBlue',
-  'MediumSlateBlue', 'DarkSlateBlue', 'Indigo', 'Purple', 'DarkMagenta', 'Blue',
-  'MediumBlue', 'DarkBlue', 'Navy', 'Thistle',
-  'Plum', 'Violet', 'Orchid', 'DarkOrchid', 'Fuchsia', 'Magenta', 'MediumOrchid',
-  'BlueViolet', 'DarkViolet', 'DarkOrchid',
-  'MediumPurple', 'Lavender', 'Gainsboro', 'LightGray', 'Silver', 'DarkGray', 'Gray',
-  'DimGray', 'LightSlateGray', 'DarkSlateGray', 'Black'
+    'LemonChiffon', 'LightGoldenrodYellow', 'PapayaWhip', 'Moccasin', 'PeachPuff', 'PaleGoldenrod', 'Bisque', 'NavajoWhite', 'Wheat', 'BurlyWood', 'Tan',
+    'Khaki', 'Yellow', 'Gold', 'Orange', 'DarkOrange', 'OrangeRed', 'Tomato', 'Coral', 'DarkSalmon', 'LightSalmon', 'LightCoral', 'Salmon', 'PaleVioletRed',
+    'Pink', 'LightPink', 'HotPink', 'DeepPink', 'MediumVioletRed', 'Crimson', 'Red', 'FireBrick', 'DarkRed', 'Maroon',
+    'Brown', 'Sienna', 'SaddleBrown', 'IndianRed', 'RosyBrown',
+    'SandyBrown', 'Goldenrod', 'DarkGoldenrod', 'Peru',
+    'Chocolate', 'DarkKhaki', 'DarkSeaGreen', 'MediumAquaMarine',
+    'MediumSeaGreen', 'SeaGreen', 'ForestGreen', 'Green', 'DarkGreen', 'OliveDrab', 'Olive', 'DarkOliveGreen', 'YellowGreen', 'LawnGreen',
+    'Chartreuse', 'GreenYellow', 'Lime', 'SpringGreen', 'LimeGreen',
+    'LightGreen', 'PaleGreen', 'PaleTurquoise',
+    'AquaMarine', 'Cyan', 'Turquoise', 'MediumTurquoise', 'DarkTurquoise', 'DeepSkyBlue',
+    'LightSeaGreen', 'CadetBlue', 'DarkCyan', 'Teal', 'Steelblue', 'LightSteelBlue', 'Honeydew', 'LightCyan',
+    'PowderBlue', 'LightBlue', 'SkyBlue', 'LightSkyBlue',
+    'DodgerBlue', 'CornflowerBlue', 'RoyalBlue', 'SlateBlue',
+    'MediumSlateBlue', 'DarkSlateBlue', 'Indigo', 'Purple', 'DarkMagenta', 'Blue',
+    'MediumBlue', 'DarkBlue', 'Navy', 'Thistle',
+    'Plum', 'Violet', 'Orchid', 'DarkOrchid', 'Fuchsia', 'Magenta', 'MediumOrchid',
+    'BlueViolet', 'DarkViolet', 'DarkOrchid',
+    'MediumPurple', 'Lavender', 'Gainsboro', 'LightGray', 'Silver', 'DarkGray', 'Gray',
+    'DimGray', 'LightSlateGray', 'DarkSlateGray', 'Black'
 ];
 
 // Variable para guardar el elemento 'color-personalizado'
 // Es decir, el que se elige con la rueda de color.
 var colorPersonalizado = document.getElementById('color-personalizado');
-
-colorPersonalizado.addEventListener('change', 
-  (function() {
-    // Se guarda el color de la rueda en colorActual
-    colorActual = colorPersonalizado.value;
-    // Completar para que cambie el indicador-de-color al colorActual
+var grillaPixeles = document.getElementById('grilla-pixeles');
+var paleta = document.getElementById('paleta');
 
 
-  })
+colorPersonalizado.addEventListener('change',
+    (function() {
+        // Se guarda el color de la rueda en colorActual
+        colorActual = colorPersonalizado.value;
+        // Completar para que cambie el indicador-de-color al colorActual
+
+
+    })
 );
+
+function crearPaletaColores() {
+    var nuevoDiv;
+    for (var i = 0; i < nombreColores.length - 1; i++) {
+        nuevoDiv = document.createElement('div');
+        nuevoDiv.className = 'color-paleta';
+        nuevoDiv.style.backgroundColor = nombreColores[i];
+        paleta.appendChild(nuevoDiv);
+    }
+}
+
+function crearGrillaPixeles() {
+    var nuevoDiv;
+    for (var i = 0; i < 1750; i++) {
+        nuevoDiv = document.createElement('div');
+        grillaPixeles.appendChild(nuevoDiv);
+    }
+}
